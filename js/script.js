f3aa15251121ff9f91f2968ed807363a38b3ce2d
rahulpandey70 <rahulpandey9103@gmail.com>
2022-01-08 16:48:16 +0530

    Modified js file

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ let toolsContainer = document.querySelector(".tools-container");
 let optionsContainer = document.querySelector(".options-container");
 let pencilContainer = document.querySelector(".pencil-container");
 let eraserContainer = document.querySelector(".eraser-container");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
 
 let flag = true;
+let pencilFlag = false;
+let eraserFlag = false;
 
 optionsContainer.addEventListener("click", (e) => {
   flag = !flag;
@@ -30,3 +34,21 @@ function closeTools() {
   pencilContainer.style.display = "none";
   eraserContainer.style.display = "none";
 }
+
+pencil.addEventListener("click", (e) => {
+    pencilFlag = !pencilFlag;
+    if(pencilFlag){
+        pencilContainer.style.display = "block";
+    }else{
+        pencilContainer.style.display = "none";
+    }
+})
+
+eraser.addEventListener("click", (e) => {
+    eraserFlag = !eraserFlag;
+    if(eraserFlag){
+        eraserContainer.style.display = "flex";
+    }else{
+        eraserContainer.style.display = "none";
+    }
+})
